3e0b5b8da5cfa759f28a2ca141677d515197cb98
954606747 <954606747@qq.com>
2018-04-02 15:53:54 +0800

    清空项目

--- springcloud-consumer/src/main/java/com/yanshaohui/springcloud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yanshaohui.springcloud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yanshaohui.springcloud.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity,Long>{
-	UserEntity findByLoginAccount(String loginAccount);
-}
